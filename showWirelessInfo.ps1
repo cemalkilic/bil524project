a8865154dfb6d52307eea0caa8a56cfc6b98f9ae
Cemal Kılıç <cemalkilic96@gmail.com>
2018-03-31 17:47:37 +0300

    done listing wifi networks if not connected to a wlan

--- showWirelessInfo.ps1
+++ showWirelessInfo.ps1
@@ -9,6 +9,7 @@ $strSignal = "Sinyal"
 $strAuthentication = "Kimlik Doðrulama"
 $strPhyAddress = "Fiziksel Adres"
 $strEncryption = "Þifre"
+$strChannel = "Kanal"
 
 # for turkish, get the wireless network info
 $data = netsh interface show interface | findstr /C:"Kablosuz"
@@ -55,6 +56,9 @@ if ($splitData[1] -eq "Baðlandý"){
     $tmpPhysicalAddr = $physicalAddrSearch -split ":"
     $physicalAddr = ($tmpPhysicalAddr)[1..$tmpPhysicalAddr.count]
     "Fiziksel Adres: " + $physicalAddr
+    
+    # todo
+    # get channel info
 
     # show network info
     # get IP address
@@ -70,4 +74,48 @@ if ($splitData[1] -eq "Baðlandý"){
     $defaultGateway = ($defaultGatewaySearch -split ":")[1].Trim() -replace "{" -replace "}"
     "Default gateway: " + $defaultGateway
     
-} 
+} Else{
+    # show available network SSIDs
+    
+    # get all the networks info
+    $networks = netsh wlan show networks mode=bssid
+    
+    # get necessary info
+    $ssids = $networks | Select-String "^SSID"
+    $encryption = $networks | Select-String $strEncryption
+    $signal = $networks | Select-String $strSignal
+    $authentication = $networks | Select-String $strAuthentication
+    $radioType = $networks | Select-String $strRadioType
+    $channel = $networks | Select-String $strChannel
+    
+    
+    # print out the header info
+    
+    "SSID`t`tSignal`tAuthentication`tEncryption`tRadioType`tChannel"
+    "----`t`t------`t--------------`t----------`t---------`t--------"
+    "`n"
+    
+    
+    For($i = 0; $i -lt $ssids.count; $i++){
+    
+        # parse the info and remove all unnecessary whitespaces 
+        $ssids[$i] = ($ssids[$i] -split ":")[1].Trim()
+        $encryption[$i] = ($encryption[$i] -split ":")[1].Trim()
+        $signal[$i] = ($signal[$i] -split ":")[1].Trim()
+        $authentication[$i] = ($authentication[$i] -split ":")[1].Trim()
+        $radioType[$i] = ($radioType[$i] -split ":")[1].Trim()
+        $channel[$i] = ($channel[$i] -split ":")[1].Trim()
+        
+        # print network info
+        $ssids[$i] + "`t" +
+        $encryption[$i] + "`t" +
+        $signal[$i] + "`t" +
+        $authentication[$i] + "`t" +
+        $radioType[$i] + "`t" +
+        $channel[$i]
+        
+    }
+    
+    
+    
+}
